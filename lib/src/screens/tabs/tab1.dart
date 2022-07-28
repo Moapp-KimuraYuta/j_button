96cbdc8198014e0c840cb4742c2202d4ec13831c
Lanway_KimuraYuta <yuta.kimura.moapp@gmail.com>
2022-07-28 16:08:09 +0900

    いい感じの余白を追加

--- lib/src/screens/tabs/tab1.dart
+++ lib/src/screens/tabs/tab1.dart
@@ -5,8 +5,24 @@ class Tab1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('tab1', style: TextStyle(fontSize: 32.0))),
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Scrollbar(
+          child: ListView(
+            padding: EdgeInsets.symmetric(
+                vertical: size.height * 0, horizontal: size.width * 0.03),
+            children: <Widget>[
+              for (int i = 0; i < 40; i++) ...{
+                Container(
+                    margin: const EdgeInsets.symmetric(vertical: 5),
+                    height: size.height * 0.07,
+                    child: ElevatedButton(onPressed: () {}, child: Text('$i'))),
+              },
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
