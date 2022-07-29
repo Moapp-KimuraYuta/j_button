bfc6cd918d74b611a1b708059c143919f94ec4be
Lanway_KimuraYuta <yuta.kimura.moapp@gmail.com>
2022-07-29 12:16:51 +0900

    ボタンにお気入り機能を追加しようと画策

--- lib/src/screens/tabs/tab1.dart
+++ lib/src/screens/tabs/tab1.dart
@@ -11,13 +11,44 @@ class Tab1 extends StatelessWidget {
         child: Scrollbar(
           child: ListView(
             padding: EdgeInsets.symmetric(
-                vertical: size.height * 0, horizontal: size.width * 0.03),
+                vertical: size.height * 0, horizontal: size.width * 0),
             children: <Widget>[
               for (int i = 0; i < 40; i++) ...{
                 Container(
-                    margin: const EdgeInsets.symmetric(vertical: 5),
+                    margin: const EdgeInsets.symmetric(vertical: 4),
                     height: size.height * 0.07,
-                    child: ElevatedButton(onPressed: () {}, child: Text('$i'))),
+                    width: 20000,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                            width: size.width * 0.15,
+                            height: size.height * 0.07,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(10),
+                                            bottomLeft: Radius.circular(10)))),
+                                onPressed: () {},
+                                child: Text('$i'))),
+                        Container(
+                          width: 2,
+                          color: Colors.black12,
+                        ),
+                        SizedBox(
+                            width: size.width * 0.75,
+                            height: size.height * 0.07,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(10),
+                                            bottomRight: Radius.circular(10)))),
+                                onPressed: () {},
+                                child: Text('$i'))),
+                      ],
+                    )),
               },
             ],
           ),
