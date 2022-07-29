5f9a5973343b7cfb556fae6028d9fa8e06f6461d
Lanway_KimuraYuta <yuta.kimura.moapp@gmail.com>
2022-07-29 11:33:57 +0900

    テストファイルのエラー修正

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:j_button/main.dart';
+import 'package:j_button/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
