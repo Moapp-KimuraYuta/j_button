4bab5257a2106f0d8002e26203b16f5a165a02e6
kimu_macmini <yuta.kimura.moapp@gmail.com>
2022-08-03 22:19:48 +0900

    設定画面を自力で作る方向に変更 URLを開けるように

--- lib/src/screens/setting.dart
+++ lib/src/screens/setting.dart
@@ -1,37 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:settings_ui/settings_ui.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-        centerTitle: true,
-      ),
-      body: SettingsList(
-        sections: [
-          SettingsSection(
-            title: const Text('SNS'),
-            tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                title: const Text('Twitter'),
-                value: const Text('@XXXXX'),
-              ),
-              SettingsTile.navigation(
-                title: const Text('Twitter'),
-                value: const Text('@XXXXX'),
-              ),
-              SettingsTile.navigation(
-                title: const Text('Twitter'),
-                value: const Text('@XXXXX'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Settings'),
+          centerTitle: true,
+        ),
+        body: ListView(
+          physics: const NeverScrollableScrollPhysics(),
+          padding: EdgeInsets.symmetric(
+              horizontal: size.width * 0.05, vertical: size.height * 0.01),
+          children: [
+            Text(
+              'Jasper7se',
+              style: TextStyle(fontSize: size.height * 0.03),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  launchUrl(Uri.parse('https://twitter.com/Jasper7se'),
+                      mode: LaunchMode.externalApplication);
+                },
+                child: Row(
+                  children: const [
+                    Text("Twitter"),
+                    Spacer(),
+                    Text("@Jasper7se"),
+                  ],
+                )),
+            ElevatedButton(
+                onPressed: () {
+                  launchUrl(Uri.parse('https://t.co/bqXympwr67'),
+                      mode: LaunchMode.externalApplication);
+                },
+                child: Row(
+                  children: const [
+                    Text("Twitch"),
+                    Spacer(),
+                    Text("jasper7se"),
+                  ],
+                ))
+          ],
+        ));
   }
 }
