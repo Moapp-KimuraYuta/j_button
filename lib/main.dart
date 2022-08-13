32e53043cd845769f22eadacd6930dedabba3b9c
kimu_macmini <yuta.kimura.moapp@gmail.com>
2022-08-13 19:38:14 +0900

    画面回転をしないように修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'src/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  //向き指定
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, //縦固定
+  ]);
   runApp(const MyApp());
 }
